666d524ac03e102e60be249f35ef97f363a3379b
corbamico <corbamico@163.com>
2020-01-17 11:31:29 +0800

    use into_future in  process_client

--- aircat-client/src/main.rs
+++ aircat-client/src/main.rs
@@ -1,9 +1,9 @@
-//use futures::future::{ready, select};
+use futures::future::{ready, select};
 use futures::StreamExt;
 
 use tokio::net::TcpStream;
 use tokio::time::{self, Duration};
-use tokio_util::codec::{BytesCodec, FramedWrite};
+use tokio_util::codec::{BytesCodec, FramedRead, FramedWrite};
 
 use bytes::Bytes;
 
@@ -20,19 +20,26 @@ async fn main() {
         0x00, 0x00, 0xBB, 0xCC, 0xDD, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x02, 0xFF, 0x23,
         0x45, 0x4E, 0x44, 0x23,
     ];
-    let stream = TcpStream::connect("127.0.0.1:9000").await.unwrap();
+    static PACKET_JSON: [u8; 35] = [
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0xBB, 0xCC, 0xDD, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x04, 0x45, 0xFF,
+        0x23, 0x45, 0x4E, 0x44, 0x23,
+    ];
+    let mut stream = TcpStream::connect("127.0.0.1:9000").await.unwrap();
+    let (rd, wr) = stream.split();
     println!("[Device]Connect to 217.0.0.1:9000");
-    let wr = FramedWrite::new(stream, BytesCodec::new());
+    let wr = FramedWrite::new(wr, BytesCodec::new());
     let interval = time::interval(Duration::from_secs(2));
 
-    interval
+    let writer = interval
         .map(move |_inst| {
-            let b = Bytes::from(&PACKET[..]);
+            let b = Bytes::from(&PACKET_JSON[..]);
             println!("[Device]Sending {:?}", b);
             Ok(b)
         })
-        .forward(wr)
-        .await
-        .map_err(|e| println!("{}", e))
-        .unwrap();
+        .forward(wr);
+    let reader = FramedRead::new(rd, BytesCodec::new())
+        .map(|x| println!("recieve: {:?}", x))
+        .collect::<()>();
+    select(reader, writer).await;
 }
--- aircat-srv-rs/src/aircat/aircatsrv.rs
+++ aircat-srv-rs/src/aircat/aircatsrv.rs
@@ -1,10 +1,10 @@
 use crate::aircat::influxdb;
 use crate::aircat::message;
-use bytes::{Buf, Bytes};
+use bytes::Bytes;
 
 use futures::{
     future::{ready, select},
-    pin_mut, stream, FutureExt, StreamExt,
+    pin_mut, FutureExt, StreamExt,
 };
 
 use hex;
@@ -18,11 +18,10 @@ use std::{
 
 use tokio::{
     self,
-    io::AsyncWriteExt,
     net::{TcpListener, TcpStream},
     sync::{mpsc, watch},
 };
-use tokio_util::codec::FramedRead;
+use tokio_util::codec::{BytesCodec, FramedRead, FramedWrite};
 
 //TODO:
 //Current, we just record last json reported from device (if there are more than one device)
@@ -33,23 +32,23 @@ lazy_static! {
 pub async fn run_aircat_srv(c: &Config, mut _rx: mpsc::Receiver<Message>) -> io::Result<()> {
     let mut listener = TcpListener::bind(&c.ServerAddr).await?;
     println!("aircat run at {}", &c.ServerAddr);
+
     //we broadcast all json to every TCP Connection of device, performance issue,
     //need handle if large number device connected.
     let (watch_tx, watch_rx) = watch::channel(Message::Nop);
-    tokio::spawn(async move {
-        loop {
-            if let Some(Message::Control(msg)) = _rx.recv().await {
-                let _ = watch_tx.broadcast(Message::Control(msg));
-            }
-        }
+    let msg_process = _rx.filter(Message::is_control).for_each(move |msg| {
+        let _ = watch_tx.broadcast(msg);
+        ready(())
     });
+    tokio::spawn(msg_process);
+
     loop {
         let (socket, client_addr) = listener.accept().await?;
         let influxdb_addr: String = c.InfluxdbServer.clone();
         let watch_rx = watch_rx.clone();
 
-        println!("aircat client connect at {}", client_addr);
         tokio::spawn(async move {
+            println!("aircat client connect at {}", client_addr);
             process_client(socket, &influxdb_addr, watch_rx).await;
             println!("aircat client disconnect, which at {}", client_addr);
         });
@@ -59,57 +58,38 @@ pub async fn run_aircat_srv(c: &Config, mut _rx: mpsc::Receiver<Message>) -> io:
 async fn process_client(
     mut socket: TcpStream,
     influxdb_addr: &str,
-    mut watch_rx: watch::Receiver<Message>,
+    watch_rx: watch::Receiver<Message>,
 ) {
-    let (rd, mut wr) = socket.split();
-    let first_packet: Arc<RwLock<Option<message::AirCatPacket>>> = Arc::new(Default::default());
-    let first_packet_writer = first_packet.clone();
+    let (rd, wr) = socket.split();
+    //step 1. we need first packet to record fixed_device field and mac field.
+    let (first, stream) = FramedRead::new(rd, message::AirCatFramedCodec::new())
+        .take_while(|p| ready(p.is_ok()))
+        .filter_map(|p| ready(p.ok()))
+        .into_future()
+        .await;
 
-    let reader = async move {
-        FramedRead::new(rd, message::AirCatFramedCodec::new())
-            .take_while(|p| futures::future::ready(p.is_ok())) //disconnect client if there is fault packet
-            .filter_map(|p| ready(p.ok())) //map to Result<AirCatPacket> to AirCatPacket
-            .zip(stream::iter(vec![true]).chain(stream::repeat(false))) //we record first packet for mac/fixed_device information
-            .map(|(p, is_first)| {
-                if is_first {
-                    let mut first = first_packet_writer.write().unwrap();
-                    *first = Some(p.clone());
-                }
-                p
-            })
-            .filter(|p| ready(!(p.msg_type == 4 && !p.json.is_empty())))
-            .for_each(|p| {
-                {
-                    let mut last = LAST_JSON.write().unwrap();
-                    *last = p.json.clone();
-                }
-                influxdb::send_json(influxdb_addr, hex::encode(&p.mac[1..7]), p.json).map(|_| ())
-            })
-            .await;
-    };
-
-    let writer = async move {
-        loop {
-            //let got = watch_rx.recv().await;
-            match watch_rx.recv().await {
-                Some(Message::Control(msg)) => {
-                    let mut bytes: Bytes = Bytes::default();
-                    {
-                        //force first_packet drop in this {}
-                        //let first_packet_writer = first_packet.clone();
-                        let first = first_packet.read().unwrap();
-                        if let Some(p) = &*first {
-                            bytes = message::gen_packet(p, msg);
-                        }
-                    }
-                    if !bytes.is_empty() {
-                        let _ = wr.write_all(bytes.bytes()).await;
-                    }
-                }
-                _ => {}
+    //step 2. read from aircat device report, and record LAST_JSON for restsrv.rs use, and send it to influxdb
+    let reader = stream
+        .filter(|p| ready(p.msg_type == 4 && !p.json.is_empty()))
+        .for_each(|p| {
+            {
+                let mut last = LAST_JSON.write().unwrap();
+                *last = p.json.clone();
             }
-        }
-    };
+            influxdb::send_json(influxdb_addr, hex::encode(&p.mac[1..7]), p.json).map(|_| ())
+        });
+    //step 3. read from watch_rx(which sent by restsrv, and is Control message), then forward to aircat device tcp session.
+    let writer = watch_rx
+        .filter(Message::is_control)
+        .map(|x| match x {
+            Message::Control(msg) => {
+                let bytes = message::gen_packet(&first.as_ref().unwrap(), msg);
+                Ok::<_, io::Error>(bytes)
+            }
+            _ => Err(io::Error::from(io::ErrorKind::InvalidInput)),
+        })
+        .forward(FramedWrite::new(wr, BytesCodec::new()));
+
     pin_mut!(reader);
     pin_mut!(writer);
     select(reader, writer).await;
@@ -133,4 +113,12 @@ pub fn load_config<T: AsRef<str>>(file: T) -> io::Result<Config> {
 pub enum Message {
     Nop,
     Control(Bytes),
+}
+impl Message {
+    fn is_control(&self) -> impl futures::Future<Output = bool> {
+        match self {
+            Message::Control(_) => ready(true),
+            _ => ready(false),
+        }
+    }
 }
